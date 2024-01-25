2b950caa5ff7a68c81d9cb4b7d6bf919efaa0cd3
Chimder <127688661+Chimder@users.noreply.github.com>
2024-01-25 21:19:41 +0200

    route

--- src/components/Streamer-info.tsx
+++ src/components/Streamer-info.tsx
@@ -1,6 +1,6 @@
 import { Channel } from 'diagnostics_channel'
 import React from 'react'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import { Emotes, TwitchStream, TwitchUser } from '@/shared/api/types'
 
 import DialogIframe from './dialog-iframe'
@@ -13,8 +13,8 @@ interface Props {
 
 export const StreamerInfo = ({ user, emotes, clips }: Props) => {
   const router = useRouter()
-  const id = router?.query?.id as string
-  const navigate = useRouter()
+  // const id = router?.query?.id as string
+  // const navigate = useRouter()
 
   const [isModalOpen, setIsModalOpen] = React.useState(false)
 
@@ -36,7 +36,7 @@ export const StreamerInfo = ({ user, emotes, clips }: Props) => {
           </div>
         ))}
       </div>
-      <section className="container z-100 flex flex-col items-center justify-center pb-10">
+      <section className="z-100 container flex flex-col items-center justify-center pb-10">
         <div className="z-[500] flex w-full flex-col items-center justify-center pt-28">
           <div className="z-[500]"></div>
           <img
@@ -53,11 +53,11 @@ export const StreamerInfo = ({ user, emotes, clips }: Props) => {
         </div>
         <div className="mt-5 flex items-center justify-center rounded-2xl border-[3px] border-border md:flex-col">
           <iframe
-            className="relative h-[68vh] w-[70vw] rounded-2xl pr-2 lg:h-[58vh] lg:w-[75vw] md:h-[50vh] md:w-[96vw] md:pb-1 md:pr-0 sm:h-[30vh]"
+            className="relative h-[68vh] w-[70vw] rounded-2xl pr-2 sm:h-[30vh] md:h-[50vh] md:w-[96vw] md:pb-1 md:pr-0 lg:h-[58vh] lg:w-[75vw]"
             src={`https://player.twitch.tv/?channel=${user?.display_name}&autoplay=1&muted=1&parent=localhost&parent=twitchers-next.vercel.app`}
           ></iframe>
           <iframe
-            className=" h-[68vh] w-[18vw] rounded-2xl lg:h-[58vh] lg:w-[21vw] md:h-[42vh] md:w-[96vw] "
+            className=" h-[68vh] w-[18vw] rounded-2xl md:h-[42vh] md:w-[96vw] lg:h-[58vh] lg:w-[21vw] "
             src={`https://www.twitch.tv/embed/${user?.display_name}/chat?parent=localhost&parent=twitchers-next.vercel.app&darkpopout`}
           ></iframe>
         </div>
